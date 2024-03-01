2ab9152440b464895a0d7f8abd3a53c38f9fa266
Vingador04 <115425342+Vingador04@users.noreply.github.com>
2024-03-01 15:09:23 +0000

    Finished implementing utils.c

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,29 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  // Checking argument vadility
+  if (lsb == NULL) return 1;
+  // Getting LSB
+  lsb = val & 0xFF;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  // Checking argument vadility
+  if (msb == NULL) return 1;
+  // Getting LSB
+  msb = (val & 0xFF00) >> 8;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t byte;
+  // Getting 4 byte info
+  if (sys_inb(port, &byte)) {
+    printf("%s returned an error!\n", __func__);
+    return 1;
+  }
+  // Retriving relevant byte
+  value = byte & 0xFF;
+  return 0;
 }
