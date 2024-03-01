e73f1f3ce3ec5d6fa1c34a2b05206ddde3ba0de0
Filipa Geraldes <up202208030@fe.up.pt>
2024-03-01 11:12:03 +0000

    Added part 2 of lab2.

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,6 +5,9 @@
 
 #include "i8254.h"
 
+int counter = 0;
+int hook = 0;
+
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   if (freq > TIMER_FREQ || timer < 0 || timer > 2) return 1; // verificar inputs
   
@@ -49,26 +52,27 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (bit_no == NULL) return 1; // apontador tem de ser válido
+  *bit_no = BIT(hook); // máscara a utilizar
+  if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook) != 0) return 1; // subscreve interrupções
+  return 0;
 }
 
 int (timer_unsubscribe_int)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (sys_irqrmpolicy(&hook) != 0) return 1; // desubscreve interrupções
+  return 0;
 }
-
-void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+ 
+void (timer_int_handler)() { // timer 0 interrupt handler
+  counter++; // increments counter
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   if (timer < 0 || timer > 2 || st == NULL) return 1; // verificar inputs
+  // TIMER_RB_CMD = 11
+  // TIMER_RB_COUNT_ = 1 (underscore final significa negado)
+  // TIMER_RB_SEL(timer) = (TIMER_0 + timer) << 1
+  // 1110???0
   uint8_t rb = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer); // read-back command
   if (sys_outb(TIMER_CTRL, rb) != 0) return 1; // escreve read-back command no timer
   if (util_sys_inb(TIMER_0 + timer, st) != 0) return 1; // ler configuração do registo do timer
