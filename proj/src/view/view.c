e86ccae7fa19aa79abab62160cb1164688b9f956
AfonsoDomingues17 <afdomingues04@gmail.com>
2024-05-20 15:55:37 +0100

    merged succesfully

--- proj/src/view/view.c
+++ proj/src/view/view.c
@@ -1,13 +1,13 @@
 #include "view.h"
 
-extern MainStateMachine mainState;
-
 extern unsigned bytes_per_pixel; /* Number of VRAM bits per pixel */
 extern unsigned h_res;
 extern unsigned v_res;
 extern uint8_t *main_buffer; //main buffer 
 extern uint8_t *secondary_buffer; //secondary buffer
 extern unsigned int vram_size; 
+
+extern MainStateMachine mainState;
 
 xpm_image_t game_background; /* Struct with background info like color */
 xpm_image_t menu_background; /* Struct with background info like color */
@@ -16,13 +16,12 @@ void (draw_frame)() {
   switch(mainState) {
     case MAIN_MENU:
       vg_draw_background((xpm_map_t) menu, &menu_background);
-      //vg_draw_sprite(mouse);
-      if(!menu_button->selected) vg_draw_sprite(menu_button);
-      else vg_draw_sprite(menu_2button);
-      if(!menu_3button->selected) vg_draw_sprite(menu_3button);
-      else vg_draw_sprite(menu_4button);
-      if(!menu_5button->selected) vg_draw_sprite(menu_5button);
-      else vg_draw_sprite(menu_6button);
+      if(!start_button_unselected->selected) vg_draw_sprite(start_button_unselected);
+      else vg_draw_sprite(start_button_selected);
+      if(!leaderboard_button_unselected->selected) vg_draw_sprite(leaderboard_button_unselected);
+      else vg_draw_sprite(leaderboard_button_selected);
+      if(!exit_button_unselected->selected) vg_draw_sprite(exit_button_unselected);
+      else vg_draw_sprite(exit_button_selected);
 
 
       break;
--- proj/src/view/view.h
+++ proj/src/view/view.h
@@ -10,15 +10,6 @@
 #include "model/xpm/xpm_background.h"
 #include "model/xpm/menu.h"
 
-/* Sprite Objects */
-extern Sprite* mouse;
-extern Sprite* menu_button;
-extern Sprite* menu_2button;
-extern Sprite* menu_3button;
-extern Sprite* redpuck;
-extern Sprite* bluepuck;
-extern Sprite* Ball;
-
 
 void (draw_frame)();
 
