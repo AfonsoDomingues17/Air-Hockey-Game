642d9a2b8effe059affc1ca3135997582385115f
Filipa Geraldes <up202208030@fe.up.pt>
2024-05-20 15:43:23 +0100

    Added xpms

---
+++ proj/src/model/xpm/game/white numbers/white_points.h
@@ -0,0 +1,13 @@
+/* XPM */
+static xpm_row_t const white_points[] = {
+/* columns rows colors chars-per-pixel */
+"27 5 2 1 ",
+"  c #FFFFFF",
+". c #FFFFFE",
+/* pixels */
+"                           ",
+"                           ",
+"                           ",
+"                           ",
+".                         ."
+};
