8655f8dac2544e8d077edeaedf985f69062e9ea9
Vingador04 <afonso.machado2004@gmail.com>
2024-05-25 16:39:33 +0100

    Implemented pixel collision

--- proj/src/model/physics.c
+++ proj/src/model/physics.c
@@ -1,5 +1,7 @@
 #include "physics.h"
 
+extern unsigned bytes_per_pixel;
+
 void move(Sprite* object, int16_t x, int16_t y, unsigned layer) {
     int new_x = object->x + x;
     int new_y = object->y - y;
@@ -43,5 +45,41 @@ bool detec_collision(Sprite* object1, int new_x, int new_y, Sprite* object2) {
   int top_border = max(new_y, object2->y);
   int bottom_border = min(new_y+object1->height, object2->y+object2->height);
 
-  return ((right_border > left_border) && (top_border < bottom_border));
+  if ((right_border >= left_border) && (top_border <= bottom_border)) {
+    printf("Finer detection called\n");
+    // Sprite Squares have overlapped move on to pixel detection
+    return pixel_detection(object1, new_x, new_y, object2, left_border, right_border, top_border, bottom_border);
+  }
+
+  return false;
+}
+
+bool pixel_detection(Sprite* object1, int new_x, int new_y, Sprite* object2, int left_border, int right_border, int top_border, int bottom_border) {
+  // Loop over every pixel inside the region
+  for (int x = left_border; x < right_border; x++) {
+    for (int y = top_border; y < bottom_border; y++) {
+      // Get color of pixel in object1
+      int pixel1_y = y - new_y;
+      int pixel1_x = x - new_x;
+      int position1 = ((object1->width*pixel1_y) + pixel1_x) * bytes_per_pixel;
+      uint32_t color1 = 0;
+      uint8_t* colors1 = object1->map;
+      colors1 += position1;
+      memcpy(&color1, colors1, bytes_per_pixel);
+      // Get color of pixel in object2
+      int pixel2_y = y - object2->y;
+      int pixel2_x = x - object2->x;
+      int position2 = ((object2->width*pixel2_y) + pixel2_x) * bytes_per_pixel;
+      uint32_t color2 = 0;
+      uint8_t* colors2 = object2->map;
+      colors2 += position2;
+      memcpy(&color2, colors2, bytes_per_pixel);
+      // Check if there is overlap
+      if (color1 != TRANSPARENT && color2 != TRANSPARENT) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
--- proj/src/model/physics.h
+++ proj/src/model/physics.h
@@ -3,6 +3,7 @@
 
 #include <lcom/lcf.h>
 #include "sprites.h"
+#include "controller/graphics/graphics.h"
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
@@ -43,5 +44,18 @@ bool detect_collision_in_layer(Sprite* object, int new_x, int new_y, unsigned la
  * @param object2 Sprite pointer to the sprite object2
 */
 bool detec_collision(Sprite* object1, int new_x, int new_y, Sprite* object2);
+
+/**
+ * @brief Returns true if the pixels inside a specific area overlapped between 2 objects
+ * @param object1 Sprite pointer to the sprite object1
+ * @param new_x Top left corner x position that the object is trying to move into
+ * @param new_y Top left corner y position that the object is trying to move into
+ * @param object2 Sprite pointer to the sprite object2
+ * @param left_border Left most border of the overlapped region
+ * @param right_border Right most border of the overlapped region
+ * @param top_border Top most border of the overlapped region
+ * @param bottom_border Bottom most border of the overlapped region
+*/
+bool pixel_detection(Sprite* object1, int new_x, int new_y, Sprite* object2, int left_border, int right_border, int top_border, int bottom_border);
 
 #endif
